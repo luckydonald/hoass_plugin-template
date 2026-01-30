843f3bf626b738b8a9082f82bafaade9d58689ec
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-01-30 15:22:46 +0100

    📄TEMPLATE | ✨ ai: [066] running… (6/X)

--- frontend_vue/eslint.base.js
+++ frontend_vue/eslint.base.js
@@ -51,7 +51,8 @@ export default [
   {
     files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
     languageOptions: {
-      parser: js.parsers.espree,
+      // Use espree parser by name for compatibility with various @eslint/js versions
+      parser: 'espree',
       ecmaVersion: 2020,
       sourceType: 'module',
     },
