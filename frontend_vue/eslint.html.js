9f59811bc9a522ae1fb9b29d84cf499d18f030c9
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-01-31 01:53:42 +0100

    📄TEMPLATE | ✨ ai: [066] running… (54/X)

--- frontend_vue/eslint.html.js
+++ frontend_vue/eslint.html.js
@@ -1,18 +1,50 @@
-import { defineConfig } from "eslint/config";
+import {defineConfig} from "eslint/config";
 import html from "@html-eslint/eslint-plugin";
 
 export default defineConfig([
-    {
-        files: [
-          "**/*.html",
-        ],
-        // When using the recommended rules (or "html/all" for all rules)
-        extends: ["html/recommended"],
-        plugins: {
-            html,
-        },
-        rules: {
-            "css-no-empty-blocks": "off",
-        },
+  {
+    files: [
+      "**/*.html",
+    ],
+    // When using the recommended rules (or "html/all" for all rules)
+    extends: ["html/recommended"],
+    plugins: {
+      html,
     },
+    rules: {
+      "css-no-empty-blocks": "off",
+    },
+  },
+  {
+    // support for in-js javascript (i.e. templates)
+    files: [
+      "**/*.html",
+      "**/*.js",
+      "**/*.ts",
+      "**/*.cjs",
+      "**/*.mjs",
+    ],
+    rules: {
+      "@html-eslint/attrs-newline": [
+        "error",
+        {
+          "closeStyle": "newline",
+          "ifAttrsMoreThan": 1,
+        },
+      ],
+      "@html-eslint/class-spacing": "error",
+      "@html-eslint/element-newline": [
+        "error",
+        {
+          "skip": [
+            "pre",
+            "code",
+          ],
+          "inline": [
+            "$inline",
+          ],
+        },
+      ],
+    },
+  },
 ]);
